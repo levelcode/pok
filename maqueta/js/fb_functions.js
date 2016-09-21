86fece02eac761a89ff54a123543b93a2404ea84
k4milo <kmilo.vanegas@gmail.com>
2016-09-21 05:28:27 -0500

    se ajusta script para tagguear usuario con el dialogo

--- maqueta/js/fb_functions.js
+++ maqueta/js/fb_functions.js
@@ -551,6 +551,7 @@ $( window ).load(function() {
               var urlformat = "https://datapola.com/"+url;
               console.log("url retornada: " + urlformat);
 
+              //Post object
               FB.api(
                 'me/objects/datapola:amigo',
                 'post',
@@ -563,85 +564,41 @@ $( window ).load(function() {
                   'fb:app_id': '1660712804256395'
                 }},
                  function(response) {
+                  //Read last object
                   FB.api(
-                  'me/datapola:reportar',
-                  'post',
-                  {
-                    'caption': 'HA SIDO REPORTADO EN DATAPOLA',
-                    'tags' : String($('#id_friend').val()),
-                    'message' : String($('#publication').val()),
-                    'amigo': String('"'+'https://datapola.com/test/index.php?id='+nombre+'"')
-                  },
-                 function(response) {
-                    if (!response || response.error) {
-                      setTimeout(function(){$('.content_terms').fadeOut(0); $('#push_public').show();$('#retorno_pub').empty();}, 3000);
-                      console.log(response.error);
-                      $('#retorno_pub').empty();
-                      $('#retorno_pub').append('Error al publicar en FB');
-                    } else {
-                      setTimeout(function(){$('.content_terms').fadeOut(0); $('#push_public').show();$('#retorno_pub').empty();}, 3000);
-                      $('#retorno_pub').empty();
-                      $('#retorno_pub').append('Éxito al publicar en FB');
-                      console.log("Éxito en la publicacion en FB" + response);
+                    'me/objects/datapola:amigo',
+                    'get',
+                    function(response) {
+                      var id_amiguito = response.data[0].id;
+
+                      //Action
+                      FB.ui({
+                        method: 'share_open_graph',
+                        action_type: 'datapola:reportar',
+                        action_properties: JSON.stringify({
+                          amigo:id_amiguito,
+                          tags:String($('#id_friend').val())
+                        })
+                      }, function(r){
+
+                        if (!response || response.error) {
+                            setTimeout(function(){$('.content_terms').fadeOut(0); $('#push_public').show();$('#retorno_pub').empty();}, 3000);
+                            console.log(response.error);
+                            $('#retorno_pub').append('Error al publicar en FB');
+                          } else {
+                            setTimeout(function(){$('.content_terms').fadeOut(0); $('#push_public').show();$('#retorno_pub').empty();}, 3000);
+                            $('#retorno_pub').empty();
+                            $('#retorno_pub').append('Éxito al publicar en FB');
+                            console.log("exito en la publicacion en FB" + response);
+                          }
+
+                      });
+
                     }
-                  }
-                );
-                  
+                  );
               });
 
-              //*********************
-              //FB Object POST METHOD 1
-              //*********************
-              /*
-              FB.api(
-              'me/objects/datapola:amigo',
-              'post',
-              {
-                object: {
-                  "og":{
-                    "url": "https://www.datapola.com/index.php?id="+nombre,
-                    "title": "HA SIDO REPORTADO EN DATAPOLA",
-                    "description": "Que todo el mundo sepa que usted le promete polas de cumpleaños a sus amigos y nunca les paga.",
-                    "app_id": "1660712804256395",
-                    "tags": $('#id_friend').val(),
-                    'amigo': "https://www.datapola.com/index.php?id="+nombre,
-                    "image": urlformat,
-                    "picture": urlformat
-                  }
-                }
-              },
-              function(response) {
-                var objectID = response.id;
-                //Action
-                FB.api(
-                    'me/datapola:reportar',
-                    'post',
-                    {
-                       amigo: "https://www.datapola.com/index.php?id="+nombre, // make sure to have the apropiate og:type meta set
-                       link: objectToLike,
-                       tags: $('#id_friend').val(), // the tokens ids for those friens you wanna tag and you got on previous step
-                       caption: 'DATAPOLA',
-                       description: 'Que todo el mundo sepa que usted le promete polas de cumpleaños a sus amigos y nunca les paga.',
-                       image: urlformat,
-                       message: $('#publication').val(),
-                       picture: urlformat
-                    },
-                    function(response) {
-                      if (!response || response.error) {
-                        setTimeout(function(){$('.content_terms').fadeOut(0); $('#push_public').show();$('#retorno_pub').empty();}, 3000);
-                        console.log(response.error);
-                        $('#retorno_pub').append('Error al publicar en FB');
-                      } else {
-                        setTimeout(function(){$('.content_terms').fadeOut(0); $('#push_public').show();$('#retorno_pub').empty();}, 3000);
-                        $('#retorno_pub').empty();
-                        $('#retorno_pub').append('Éxito al publicar en FB');
-                        console.log("exito en la publicacion en FB" + response);
-                      }
-                      
-                    }
-                );
-              });
-              */
+              
               //*********************
               //FB Object POST METHOD
               //*********************
