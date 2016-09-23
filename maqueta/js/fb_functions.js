74ee55c4b9c4a32d5a656a190b68b7fa4b9bcc12
levelcode <leonardopalomino@MacBook-Pro-de-Leo.local>
2016-09-23 03:13:28 -0500

    mobile fix Leo

--- maqueta/js/fb_functions.js
+++ maqueta/js/fb_functions.js
@@ -3,6 +3,35 @@ var canvas ='';
 var context ='';
 var imageLoader = '';
 var random = Math.floor((Math.random() * 10000000) + 1);
+//Facebook
+var url_l = String((window.location != window.parent.location) ? document.referrer: document.location.href);
+var dominio = "datapola.com/";
+var movil = false;
+var ios =  iOS();
+var downloadURL = "";
+var day;
+var month;
+var year;
+
+//Vars FB
+var url_l = String((window.location != window.parent.location) ? document.referrer: document.location.href),
+body = $('body'),
+dominio = "datapola.com/",
+movil = false,
+ios =  iOS(),
+ref = document.referrer,
+fb_valid = false;
+
+
+(function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
+
+movil = jQuery.browser.mobile;
+
+if(movil==true){
+  console.log("desde mobile: "+movil);
+}else{
+
+};
 
 //Variables de video
 var video2c = $("#vid2"),
@@ -17,17 +46,42 @@ var video2 = $("#vid2").get(0),
 
 //Gonzo script
 
+function iOS() {
+
+  var iDevices = [
+    'iPad Simulator',
+    'iPhone Simulator',
+    'iPod Simulator',
+    'iPad',
+    'iPhone',
+    'iPod'
+  ];
+
+  while (iDevices.length) {
+    if (navigator.platform === iDevices.pop()){ return true; }
+  }
+
+  return false;
+}
+
 //hack for mobile video
 function autov(video, videoc){
   videoc.prop('muted', true);
   video.play();
-  setTimeout(function(){ video.pause();videoc.prop('muted', false);video.currentTime = 0;}, 500);
+  setTimeout(function(){ video.pause();videoc.prop('muted', false);video.currentTime = 0;}, 200);
 }
 
 $( window ).load(function() {
 
     //**************
     //Click and save
+    if(movil == true){
+      $('#save').on("touchstart",function(){
+        console.log("Reproduce touch en mobile");
+        autov(video, videoc);
+      });
+    }
+    
     $('#save').click(function() {
 
       FB.login(function(response) {
@@ -35,7 +89,9 @@ $( window ).load(function() {
       }, {scope: 'publish_actions,user_friends'});
 
       //hack for mobile video manage autoplay
-      autov(video, videoc);
+      if(movil != true){
+        autov(video, videoc);
+      }
       //video.play();
       //setTimeout(function(){ video.pause();video.currentTime = 0;}, 200);
       $('.loader').show();
@@ -71,24 +127,7 @@ $( window ).load(function() {
 
     //**************
 
-    //Facebook
-    var url_l = String((window.location != window.parent.location) ? document.referrer: document.location.href);
-    var dominio = "datapola.com/";
-    var movil = false;
-    var ios =  iOS();
-    var downloadURL = "";
-    var day;
-    var month;
-    var year;
-
-    //Vars FB
-    var url_l = String((window.location != window.parent.location) ? document.referrer: document.location.href),
-    body = $('body'),
-    dominio = "datapola.com/",
-    movil = false,
-    ios =  iOS(),
-    ref = document.referrer,
-    fb_valid = false;
+    
 
 
     //******************
@@ -96,15 +135,7 @@ $( window ).load(function() {
     //******************
 
 
-    (function(a){(jQuery.browser=jQuery.browser||{}).mobile=/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))})(navigator.userAgent||navigator.vendor||window.opera);
 
-    movil = jQuery.browser.mobile;
-
-    if(movil==true){
-
-    }else{
-
-    };
     
 
 
@@ -113,40 +144,6 @@ $( window ).load(function() {
     //Scale
     //****************
 
-    
-    var h_video, w_video, h_foto, w_foto
-    $(window).resize(function(){
-        h_video = $('.content_video');
-        console.log();
-    });
-    function ScaleVideo_foto(){
-    }
-    
-
-    var $el = $(".content_video");
-    var elHeight = $el.outerHeight();
-    var elWidth = $el.outerWidth();
-
-    var $wrapper = $("#scaleable-wrapper");
-
-    $(window).resize(function(){
-        console.log()
-
-    });
-
-    function size(){
-
-    }
-
-    function doResize(ui) { 
-      var scale, origin; 
-      scale = Math.min(ui.width / elWidth, ui.height / elHeight);
-      
-      $el.css({
-        transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
-      });
-      return scale;
-    };
     
 
     //****************
@@ -184,9 +181,14 @@ $( window ).load(function() {
           if (response.status == 'connected') {
             //onLogin(response);
             FB.login(function(response) {
-              onLogin(response);
-              console.log("Ingreso Automatico");
-              $(".content_facebook_connect").hide();
+              if(movil == false){
+                onLogin(response);
+                console.log("Ingreso Automatico");
+                $(".content_facebook_connect").hide();
+              }else{
+                console.log("Ingreso por Mobile");
+              }
+              
             }, {scope: 'publish_actions,user_friends'});
           } else {
             console.log("Ingreso por Click");
@@ -251,23 +253,6 @@ $( window ).load(function() {
           }
           return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
     }
-    function iOS() {
-
-      var iDevices = [
-        'iPad Simulator',
-        'iPhone Simulator',
-        'iPod Simulator',
-        'iPad',
-        'iPhone',
-        'iPod'
-      ];
-
-      while (iDevices.length) {
-        if (navigator.platform === iDevices.pop()){ return true; }
-      }
-
-      return false;
-    }
 
     function init(){
 
@@ -302,6 +287,7 @@ $( window ).load(function() {
 
       //Global linked image
       clickReport.click(function(event) {
+        $("body").scrollTop();
         $('.loader').show();
         //Hack videos mobile
 
